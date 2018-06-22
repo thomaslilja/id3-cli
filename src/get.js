af9072a1f0c6e2a579b748c14250a06c0ebc7334
Thomas Lilja <thomas.lilja@aller.com>
2018-06-22 14:32:24 +0200

    Added get command

--- src/get.js
+++ src/get.js
@@ -15,6 +15,7 @@ export default (file) => {
         console.log(chalk.red(`\nFailed to read tags on '${file}'.\n`));
         process.exit(1);
       }
+      console.log(chalk.blue(`Tags for '${file}'\n`));
       const tagsToDisplay = pick(tags, ['title', 'artist', 'album', 'year']);
       foreach(tagsToDisplay, (value, key) => {
         console.log(`${chalk.green(`${key.replace(/\b\w/g, l => l.toUpperCase())}:`)} ${chalk.white(value)}`);
--- test/index.js
+++ test/index.js
@@ -1,9 +1,8 @@
 import assert from 'assert';
 
-describe('Array', () => {
-  describe('#indexOf()', () => {
-    it('should return -1 when the value is not present', () => {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+// TODO: Add actual tests.
+describe('ID3-CLI', () => {
+  it('should return true', () => {
+    assert.equal(true, true);
   });
 });
